8ee1910fd8def81c2a5c8b6fdde4b99bb3d278a3
RebeccaGagblezu <rebeccagagblezu@gmail.com>
2017-07-28 18:35:10 +0100

    Code Modification

---
+++ js/gallery.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($) {
+ 
+        $('#myCarousel').carousel({
+                interval: 5000
+        });
+ 
+        //Handles the carousel thumbnails
+        $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#myCarouselGal').carousel(parseInt(id));
+        } catch (e) {
+            console.log('Regex failed!', e);
+        }
+    });
+        // When the carousel slides, auto update the text
+        $('#myCarouselGal').on('slid.bs.carousel', function (e) {
+                 var id = $('.item2.active').data('slide-number');
+                $('#carousel-text').html($('#slide-content-'+id).html());
+        });
+});
